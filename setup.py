ebcb178e7765b11b73681f1507e4b4c70ad12796
dhondta <alexandre.dhondt@gmail.com>
2018-03-04 11:43:39 +0100

    Updated PyPi package (2)

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 setup(
   name = "webgrep-tool",
-  version = "1.7",
+  version = "1.8",
   license = "GPLv3",
   description = "Web page Grep-like tool with additional features like JS deobfuscation and easy extensibility",
   author = "Alexandre D\'Hondt",
   author_email = "alexandre.dhondt@gmail.com",
   url = "https://github.com/dhondta/webgrep",
   keywords = ["grep", "webpage", "js-deobfuscator", "css-unminifier",
-              "exif-metadata", "ocr"],
+              "exif-metadata", "ocr", "steghide"],
   scripts = ["webgrep"],
   python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
   install_requires=[str(i).split("(", 1)[0].strip() for i in \
