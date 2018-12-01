0cfddd826c20220a589874dc14600c14eef0833a
dhondta <alexandre.dhondt@gmail.com>
2018-12-01 15:41:05 +0100

    New release

--- setup.py
+++ setup.py
@@ -1,20 +1,50 @@
-from pip.download import PipSession as Pip
-from pip.req import parse_requirements as parse
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from os.path import abspath, dirname, join
 from setuptools import setup
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
+currdir = abspath(dirname(__file__))
+with open(join(currdir, 'README.md')) as f:
+    long_descr = f.read()
+
+requirements = parse_requirements("requirements.txt", session=False)
 setup(
   name = "webgrep-tool",
-  version = "1.13",
-  license = "GPLv3",
-  description = "Web page Grep-like tool with additional features like JS deobfuscation and easy extensibility",
   author = "Alexandre D\'Hondt",
   author_email = "alexandre.dhondt@gmail.com",
   url = "https://github.com/dhondta/webgrep",
+  version = "1.13",
+  license = "GPLv3",
+  description = "Web page Grep-like tool with additional features like JS "
+                "deobfuscation and easy extensibility",
+  long_description=long_descr,
+  long_description_content_type='text/markdown',
   keywords = ["grep", "webpage", "js-deobfuscator", "css-unminifier",
               "exif-metadata", "ocr", "steghide"],
   scripts = ["webgrep"],
-  python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-  install_requires=[str(i).split("(", 1)[0].strip() for i in \
-                    parse("requirements.txt", session=Pip())],
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Topic :: Security',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+  install_requires=[str(r.req) for r in requirements],
+  python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<4',
 )
